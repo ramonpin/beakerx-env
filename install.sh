d1ccd4571424156445d404aba2e11c14af37e6d2
Ramón Pin <ramon.pin@gmail.com>
2020-03-11 16:00:53 +0100

    Added hide_code extension

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 pip install -r beakerx.txt
+pip install jupyter_contrib_nbextensions
+pip install jupyterthemes
 beakerx install --lab
 jupyter nbextension enable --py widgetsnbextension
+jupyter-contrib nbextension install --sys-prefix
+## jt -t theme para activar un tema
+pip install hide_code
+jupyter nbextension install --prefix ~/virtualenvs/beakerx  --py hide_code
+jupyter nbextension enable hide_code --py
+jupyter serverextension enable --py hide_code
